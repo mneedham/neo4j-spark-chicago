86f3c51fdff5c0dc958fb17726e226358c42dd9e
Mark Needham <mark.needham@neotechnology.com>
2015-04-14 20:38:12 +0200

    updating readme with michael's updates

---
+++ import.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+DATA=${1-/tmp}
+NEO=${2-./neo4j-enterprise-2.2.0}
+$NEO/bin/neo4j-import \
+--into $DATA/crimes.db \
+--nodes $DATA/crimes.csv \
+--nodes $DATA/beats.csv \
+--nodes $DATA/primaryTypes.csv \
+--relationships $DATA/crimesBeats.csv \
+--relationships $DATA/crimesPrimaryTypes.csv
