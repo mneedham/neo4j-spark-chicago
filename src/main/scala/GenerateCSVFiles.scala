30cb0533194ee138f029708ee3d1cb2274f43a12
Mark Needham <mark.needham@neotechnology.com>
2015-04-14 08:24:05 +0200

    updating README

--- src/main/scala/GenerateCSVFiles.scala
+++ src/main/scala/GenerateCSVFiles.scala
@@ -6,7 +6,7 @@ import org.apache.hadoop.fs._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-object CrimeApp3 {
+object GenerateCSVFiles {
 
   def merge(srcPath: String, dstPath: String, header: String): Unit =  {
     val hadoopConfig = new Configuration()
@@ -15,12 +15,12 @@ object CrimeApp3 {
   }
 
   def main(args: Array[String]) {
-    // https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-present/ijzp-q8t2
-    var crimeFile = "/Users/markneedham/Downloads/Crimes_-_2001_to_present.csv"
+    var crimeFile = System.getenv("CSV_FILE")
 
-    if(args.length >= 1) {
-      crimeFile = args(0)
+    if(crimeFile == null || !new File(crimeFile).exists()) {
+      throw new RuntimeException("Cannot find CSV file [" + crimeFile + "]")
     }
+
     println("Using %s".format(crimeFile))
 
     val conf = new SparkConf().setAppName("Chicago Crime Dataset")
@@ -29,24 +29,11 @@ object CrimeApp3 {
     val crimeData = sc.textFile(crimeFile).cache()
     val withoutHeader: RDD[String] = dropHeader(crimeData)
 
-    val initialFile = sc.broadcast(withoutHeader)
-
     generateFile("/tmp/primaryTypes.csv", withoutHeader, columns => Array(columns(5).trim(), "CrimeType"), "crimeType:ID(CrimeType),:LABEL")
     generateFile("/tmp/beats.csv", withoutHeader, columns => Array(columns(10), "Beat"), "id:ID(Beat),:LABEL")
     generateFile("/tmp/crimes.csv", withoutHeader, columns => Array(columns(0),"Crime", columns(2), columns(6)), "id:ID(Crime),:LABEL,date,description", false)
     generateFile("/tmp/crimesPrimaryTypes.csv", withoutHeader, columns => Array(columns(0),columns(5).trim(), "CRIME_TYPE"), ":START_ID(Crime),:END_ID(CrimeType),:TYPE")
     generateFile("/tmp/crimesBeats.csv", withoutHeader, columns => Array(columns(0),columns(10), "ON_BEAT"), ":START_ID(Crime),:END_ID(Beat),:TYPE")
-  }
-
-  def transformRDD(withoutHeader: RDD[String], fn: Array[String] => Array[String], header: String , fileName:String, separator: String = ",") = {
-    print("transforming RDD... ")
-    (withoutHeader.mapPartitions(lines => {
-      val parser = new CSVParser(',')
-      lines.map(line => {
-        val columns = parser.parseLine(line)
-        fn(columns).mkString(separator)
-      })
-    }).distinct(), header, fileName)
   }
 
   def generateFile(file: String, withoutHeader: RDD[String], fn: Array[String] => Array[String], header: String , distinct:Boolean = true, separator: String = ",") = {
